ee4793d7122d086ac3759d160b7cf38fcc014496
Marcus <marcusyoung0603@gmail.com>
2019-11-04 14:45:23 -0800

    idk why to put here

--- server/sqlServer.js
+++ server/sqlServer.js
@@ -18,24 +18,25 @@ app.listen(port, () =>
   console.log(`Nike Review Component listening on portxgdfsgdfsg ${port}!`)
 );
 
-app.get('/api/reviews', (req, res) => {
-  console.log('recieved get request');
-  res.send('now this is podracing');
-})
+// app.get('/api/reviews', (req, res) => {
+//   console.log('recieved get request');
+//   res.send('now this is podracing');
+// })
 
-app.get('/loaderio-9263376403b50a059029213754df6b48', (req, res) {
+app.get('/loaderio-9263376403b50a059029213754df6b48', (req, res) => {
   res.send('loaderio-9263376403b50a059029213754df6b48');
 })
 
 app.get('/api/reviews', (req, res) => {
   //console.log(req);
+  console.log('recieving real get request');
   getFromSql(req.query.shoe_id).then(function(prod) {
     res.send(prod);
     console.log('server side get request complete')
   })
-  getFromSql(req.body).then(res.end(),
-    console.log('document saved')
-  );  
+  // getFromSql(req.body).then(res.end(),
+  //   console.log('document saved')
+  // );  
 });
 
 app.post('/api/reviews', (req, res) => {
